7ec6538f847c79603a076d33c023f8c45d8dddce
tomkamin <tom.kamin@sitkatech.com>
2025-08-27 18:00:31 -0700

    WADNR-2189 Move FHT to .NET Core updating gitignore and adding new files from project creation

---
+++ WADNRForestHealthTracker.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WADNRForestHealthTracker.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
