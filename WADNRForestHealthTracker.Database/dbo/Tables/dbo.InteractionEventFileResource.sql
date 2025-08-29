e9bce3839bea290834b886e978f90158d08e9db8
tomkamin <tom.kamin@sitkatech.com>
2025-08-29 15:48:33 -0700

    WADNR-2189 Move FHT to .NET Core adding and updating the table scripts InteractionEvent tables 2

--- WADNRForestHealthTracker.Database/dbo/Tables/dbo.InteractionEventFileResource.sql
+++ WADNRForestHealthTracker.Database/dbo/Tables/dbo.InteractionEventFileResource.sql
@@ -1,26 +1,9 @@
 CREATE TABLE [dbo].[InteractionEventFileResource](
-	[InteractionEventFileResourceID] [int] IDENTITY(1,1) NOT NULL,
-	[InteractionEventID] [int] NOT NULL,
-	[FileResourceID] [int] NOT NULL,
+	[InteractionEventFileResourceID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_InteractionEventFileResource_InteractionEventFileResourceID] PRIMARY KEY,
+	[InteractionEventID] [int] NOT NULL CONSTRAINT [FK_InteractionEventFileResource_InteractionEvent_InteractionEventID] FOREIGN KEY REFERENCES [dbo].[InteractionEvent]([InteractionEventID]),
+	[FileResourceID] [int] NOT NULL CONSTRAINT [FK_InteractionEventFileResource_FileResource_FileResourceID] FOREIGN KEY REFERENCES [dbo].[FileResource]([FileResourceID]) ON DELETE CASCADE,
 	[DisplayName] [varchar](200) NOT NULL,
 	[Description] [varchar](1000) NULL,
- CONSTRAINT [PK_InteractionEventFileResource_InteractionEventFileResourceID] PRIMARY KEY CLUSTERED 
-(
-	[InteractionEventFileResourceID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF),
- CONSTRAINT [AK_InteractionEventFileResource_FileResourceID] UNIQUE NONCLUSTERED 
-(
-	[FileResourceID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF)
+ CONSTRAINT [AK_InteractionEventFileResource_FileResourceID] UNIQUE ([FileResourceID])
 ) 
 GO
-ALTER TABLE [dbo].[InteractionEventFileResource]  WITH CHECK ADD  CONSTRAINT [FK_InteractionEventFileResource_FileResource_FileResourceID] FOREIGN KEY([FileResourceID])
-REFERENCES [dbo].[FileResource] ([FileResourceID])
-ON DELETE CASCADE
-GO
-ALTER TABLE [dbo].[InteractionEventFileResource] CHECK CONSTRAINT [FK_InteractionEventFileResource_FileResource_FileResourceID]
-GO
-ALTER TABLE [dbo].[InteractionEventFileResource]  WITH CHECK ADD  CONSTRAINT [FK_InteractionEventFileResource_InteractionEvent_InteractionEventID] FOREIGN KEY([InteractionEventID])
-REFERENCES [dbo].[InteractionEvent] ([InteractionEventID])
-GO
-ALTER TABLE [dbo].[InteractionEventFileResource] CHECK CONSTRAINT [FK_InteractionEventFileResource_InteractionEvent_InteractionEventID]
