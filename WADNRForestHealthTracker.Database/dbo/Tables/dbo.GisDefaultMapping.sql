dbbd19bbce052b1c773ee6ff58759c530bfd33a4
tomkamin <tom.kamin@sitkatech.com>
2025-08-29 15:29:51 -0700

    WADNR-2189 Move FHT to .NET Core adding and updating the table scripts Gis*

--- WADNRForestHealthTracker.Database/dbo/Tables/dbo.GisDefaultMapping.sql
+++ WADNRForestHealthTracker.Database/dbo/Tables/dbo.GisDefaultMapping.sql
@@ -1,25 +1,7 @@
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE [dbo].[GisDefaultMapping](
-	[GisDefaultMappingID] [int] IDENTITY(1,1) NOT NULL,
-	[GisUploadSourceOrganizationID] [int] NOT NULL,
-	[FieldDefinitionID] [int] NOT NULL,
-	[GisDefaultMappingColumnName] [varchar](300) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
- CONSTRAINT [PK_GisDefaultMapping_GisDefaultMappingID] PRIMARY KEY CLUSTERED 
-(
-	[GisDefaultMappingID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-
+    [GisDefaultMappingID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_GisDefaultMapping_GisDefaultMappingID] PRIMARY KEY,
+    [GisUploadSourceOrganizationID] [int] NOT NULL CONSTRAINT [FK_GisDefaultMapping_GisUploadSourceOrganization_GisUploadSourceOrganizationID] FOREIGN KEY REFERENCES [dbo].[GisUploadSourceOrganization]([GisUploadSourceOrganizationID]),
+    [FieldDefinitionID] [int] NOT NULL CONSTRAINT [FK_GisDefaultMapping_FieldDefinition_FieldDefinitionID] FOREIGN KEY REFERENCES [dbo].[FieldDefinition]([FieldDefinitionID]),
+    [GisDefaultMappingColumnName] [varchar](300) NOT NULL
+)
 GO
-ALTER TABLE [dbo].[GisDefaultMapping]  WITH CHECK ADD  CONSTRAINT [FK_GisDefaultMapping_FieldDefinition_FieldDefinitionID] FOREIGN KEY([FieldDefinitionID])
-REFERENCES [dbo].[FieldDefinition] ([FieldDefinitionID])
-GO
-ALTER TABLE [dbo].[GisDefaultMapping] CHECK CONSTRAINT [FK_GisDefaultMapping_FieldDefinition_FieldDefinitionID]
-GO
-ALTER TABLE [dbo].[GisDefaultMapping]  WITH CHECK ADD  CONSTRAINT [FK_GisDefaultMapping_GisUploadSourceOrganization_GisUploadSourceOrganizationID] FOREIGN KEY([GisUploadSourceOrganizationID])
-REFERENCES [dbo].[GisUploadSourceOrganization] ([GisUploadSourceOrganizationID])
-GO
-ALTER TABLE [dbo].[GisDefaultMapping] CHECK CONSTRAINT [FK_GisDefaultMapping_GisUploadSourceOrganization_GisUploadSourceOrganizationID]
--- WADNRForestHealthTracker.Database/dbo/Tables/dbo.GisFeature.sql
+++ WADNRForestHealthTracker.Database/dbo/Tables/dbo.GisFeature.sql
@@ -1,22 +1,9 @@
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE [dbo].[GisFeature](
-	[GisFeatureID] [int] IDENTITY(1,1) NOT NULL,
-	[GisUploadAttemptID] [int] NOT NULL,
-	[GisFeatureGeometry] [geometry] NOT NULL,
-	[GisImportFeatureKey] [int] NOT NULL,
-	[IsValid]  AS ([GisFeatureGeometry].[STIsValid]()),
-	[CalculatedArea] [decimal](38, 20) NULL,
- CONSTRAINT [PK_GisFeature_GisFeatureID] PRIMARY KEY CLUSTERED 
-(
-	[GisFeatureID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-
+    [GisFeatureID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_GisFeature_GisFeatureID] PRIMARY KEY,
+    [GisUploadAttemptID] [int] NOT NULL CONSTRAINT [FK_GisFeature_GisUploadAttempt_GisUploadAttemptID] FOREIGN KEY REFERENCES [dbo].[GisUploadAttempt]([GisUploadAttemptID]),
+    [GisFeatureGeometry] [geometry] NOT NULL,
+    [GisImportFeatureKey] [int] NOT NULL,
+    [IsValid]  AS ([GisFeatureGeometry].[STIsValid]()),
+    [CalculatedArea] [decimal](38, 20) NULL
+)
 GO
-ALTER TABLE [dbo].[GisFeature]  WITH CHECK ADD  CONSTRAINT [FK_GisFeature_GisUploadAttempt_GisUploadAttemptID] FOREIGN KEY([GisUploadAttemptID])
-REFERENCES [dbo].[GisUploadAttempt] ([GisUploadAttemptID])
-GO
-ALTER TABLE [dbo].[GisFeature] CHECK CONSTRAINT [FK_GisFeature_GisUploadAttempt_GisUploadAttemptID]
