60a04814b136b61bb2dc2eb99e4d819f81075a04
tomkamin <tom.kamin@sitkatech.com>
2025-08-29 18:37:58 -0700

    WADNR-2189 Move FHT to .NET Core adding and updating the table scripts Socrata and role tables

--- WADNRForestHealthTracker.Database/dbo/Tables/dbo.SocrataDataMartRawJsonImportTableType.sql
+++ WADNRForestHealthTracker.Database/dbo/Tables/dbo.SocrataDataMartRawJsonImportTableType.sql
@@ -1,16 +1,5 @@
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE [dbo].[SocrataDataMartRawJsonImportTableType](
-	[SocrataDataMartRawJsonImportTableTypeID] [int] NOT NULL,
-	[SocrataDataMartRawJsonImportTableTypeName] [varchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
- CONSTRAINT [PK_SocrataDataMartRawJsonImportTableType_SocrataDataMartRawJsonImportTableTypeID] PRIMARY KEY CLUSTERED 
-(
-	[SocrataDataMartRawJsonImportTableTypeID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
- CONSTRAINT [AK_SocrataDataMartRawJsonImportTableType_SocrataDataMartRawJsonImportTableTypeName] UNIQUE NONCLUSTERED 
-(
-	[SocrataDataMartRawJsonImportTableTypeName] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+    [SocrataDataMartRawJsonImportTableTypeID] [int] NOT NULL CONSTRAINT [PK_SocrataDataMartRawJsonImportTableType_SocrataDataMartRawJsonImportTableTypeID] PRIMARY KEY,
+    [SocrataDataMartRawJsonImportTableTypeName] [varchar](100) NOT NULL CONSTRAINT [AK_SocrataDataMartRawJsonImportTableType_SocrataDataMartRawJsonImportTableTypeName] UNIQUE
+)
+GO
