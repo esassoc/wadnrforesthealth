7d00aefd34f849d662d6e65b197a36d06ec91cc5
tomkamin <tom.kamin@sitkatech.com>
2025-08-29 17:55:38 -0700

    WADNR-2189 Move FHT to .NET Core adding and updating the table scripts Project* tables 4

--- WADNRForestHealthTracker.Database/dbo/Tables/dbo.ProjectType.sql
+++ WADNRForestHealthTracker.Database/dbo/Tables/dbo.ProjectType.sql
@@ -1,24 +1,6 @@
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE [dbo].[ProjectType](
-	[ProjectTypeID] [int] IDENTITY(1,1) NOT NULL,
-	[TaxonomyBranchID] [int] NOT NULL,
-	[ProjectTypeName] [varchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	[ProjectTypeDescription] [dbo].[html] NULL,
-	[ProjectTypeCode] [varchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[ThemeColor] [varchar](7) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[ProjectTypeSortOrder] [int] NULL,
-	[LimitVisibilityToAdmin] [bit] NOT NULL,
- CONSTRAINT [PK_ProjectType_ProjectTypeID] PRIMARY KEY CLUSTERED 
-(
-	[ProjectTypeID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-
+    [ProjectTypeID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_ProjectType_ProjectTypeID] PRIMARY KEY,
+    [ProjectTypeName] [varchar](100) NOT NULL CONSTRAINT [AK_ProjectType_ProjectTypeName] UNIQUE,
+    [ProjectTypeDisplayName] [varchar](100) NOT NULL CONSTRAINT [AK_ProjectType_ProjectTypeDisplayName] UNIQUE
+)
 GO
-ALTER TABLE [dbo].[ProjectType]  WITH CHECK ADD  CONSTRAINT [FK_ProjectType_TaxonomyBranch_TaxonomyBranchID] FOREIGN KEY([TaxonomyBranchID])
-REFERENCES [dbo].[TaxonomyBranch] ([TaxonomyBranchID])
-GO
-ALTER TABLE [dbo].[ProjectType] CHECK CONSTRAINT [FK_ProjectType_TaxonomyBranch_TaxonomyBranchID]
--- WADNRForestHealthTracker.Database/dbo/Tables/dbo.ProjectUpdateConfiguration.sql
+++ WADNRForestHealthTracker.Database/dbo/Tables/dbo.ProjectUpdateConfiguration.sql
@@ -1,20 +1,13 @@
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE [dbo].[ProjectUpdateConfiguration](
-	[ProjectUpdateConfigurationID] [int] IDENTITY(1,1) NOT NULL,
-	[ProjectUpdateKickOffDate] [datetime] NULL,
-	[ProjectUpdateCloseOutDate] [datetime] NULL,
-	[ProjectUpdateReminderInterval] [int] NULL,
-	[EnableProjectUpdateReminders] [bit] NOT NULL,
-	[SendPeriodicReminders] [bit] NOT NULL,
-	[SendCloseOutNotification] [bit] NOT NULL,
-	[ProjectUpdateKickOffIntroContent] [dbo].[html] NULL,
-	[ProjectUpdateReminderIntroContent] [dbo].[html] NULL,
-	[ProjectUpdateCloseOutIntroContent] [dbo].[html] NULL,
- CONSTRAINT [PK_ProjectUpdateConfiguration_ProjectUpdateConfigurationID] PRIMARY KEY CLUSTERED 
-(
-	[ProjectUpdateConfigurationID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+    [ProjectUpdateConfigurationID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_ProjectUpdateConfiguration_ProjectUpdateConfigurationID] PRIMARY KEY,
+    [ProjectUpdateKickOffDate] [datetime] NULL,
+    [ProjectUpdateCloseOutDate] [datetime] NULL,
+    [ProjectUpdateReminderInterval] [int] NULL,
+    [EnableProjectUpdateReminders] [bit] NOT NULL,
+    [SendPeriodicReminders] [bit] NOT NULL,
+    [SendCloseOutNotification] [bit] NOT NULL,
+    [ProjectUpdateKickOffIntroContent] [dbo].[html] NULL,
+    [ProjectUpdateReminderIntroContent] [dbo].[html] NULL,
+    [ProjectUpdateCloseOutIntroContent] [dbo].[html] NULL
+)
+GO
