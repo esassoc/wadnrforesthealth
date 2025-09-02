90e8e2dbe464169f3838bca8d3436b21eb29e0e9
tomkamin <tom.kamin@sitkatech.com>
2025-09-02 16:39:43 -0700

    WADNR-2189 Move FHT to .NET Core adding and updating the table scripts tmp* tables

--- WADNRForestHealthTracker.Database/dbo/Tables/dbo.tmpAgreement.sql
+++ WADNRForestHealthTracker.Database/dbo/Tables/dbo.tmpAgreement.sql
@@ -1,28 +1,24 @@
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE [dbo].[tmpAgreement](
 	[TmpAgreementID] [int] IDENTITY(1,1) NOT NULL,
-	[AgreementType] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[AgreementNumber] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[SourceOfFunding] [nvarchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[AgencyEntity] [nvarchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[PM] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[ProgramIndexProjectCode] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[StartDate] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[EndDate] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[AgreementAmount] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[Expended] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[Balance] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[INCLUDED_IN_BALANCE_INV_AMT_SUBMITTED_ON_7_12_OR_AFTER] [nvarchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[Region_Div] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[ACTIVITY] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[1ST_BILL_DUE_ON] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[Notes] [nvarchar](150) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[PARTNER_CONTACT] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[AgreementType] [nvarchar](50) NULL,
+	[AgreementNumber] [nvarchar](50) NULL,
+	[SourceOfFunding] [nvarchar](100) NULL,
+	[AgencyEntity] [nvarchar](100) NULL,
+	[PM] [nvarchar](50) NULL,
+	[ProgramIndexProjectCode] [nvarchar](50) NULL,
+	[StartDate] [nvarchar](50) NULL,
+	[EndDate] [nvarchar](50) NULL,
+	[AgreementAmount] [nvarchar](50) NULL,
+	[Expended] [nvarchar](50) NULL,
+	[Balance] [nvarchar](50) NULL,
+	[INCLUDED_IN_BALANCE_INV_AMT_SUBMITTED_ON_7_12_OR_AFTER] [nvarchar](1) NULL,
+	[Region_Div] [nvarchar](50) NULL,
+	[ACTIVITY] [nvarchar](50) NULL,
+	[1ST_BILL_DUE_ON] [nvarchar](50) NULL,
+	[Notes] [nvarchar](150) NULL,
+	[PARTNER_CONTACT] [nvarchar](50) NULL,
  CONSTRAINT [PK_tmpAgreement_TmpAgreementID] PRIMARY KEY CLUSTERED 
 (
 	[TmpAgreementID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+)
+
