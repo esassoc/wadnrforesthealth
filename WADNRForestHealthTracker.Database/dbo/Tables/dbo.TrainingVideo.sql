ef1a3c002ae1a8be04b1d990e2c2786e5191ed2d
tomkamin <tom.kamin@sitkatech.com>
2025-09-04 15:21:40 -0700

    WADNR-2189 Move FHT to .NET Core adding and updating the table scripts for Training, Vendor, and WA legis district tables

--- WADNRForestHealthTracker.Database/dbo/Tables/dbo.TrainingVideo.sql
+++ WADNRForestHealthTracker.Database/dbo/Tables/dbo.TrainingVideo.sql
@@ -1,15 +1,8 @@
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE [dbo].[TrainingVideo](
-	[TrainingVideoID] [int] IDENTITY(1,1) NOT NULL,
-	[VideoName] [varchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	[VideoDescription] [varchar](500) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[VideoUploadDate] [datetime] NOT NULL,
-	[VideoURL] [varchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
- CONSTRAINT [PK_TrainingVideo_TrainingVideoID] PRIMARY KEY CLUSTERED 
-(
-	[TrainingVideoID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+    [TrainingVideoID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_TrainingVideo_TrainingVideoID] PRIMARY KEY,
+    [VideoName] [varchar](100) NOT NULL,
+    [VideoDescription] [varchar](500) NULL,
+    [VideoUploadDate] [datetime] NOT NULL,
+    [VideoURL] [varchar](100) NOT NULL
+)
+GO
--- WADNRForestHealthTracker.Database/dbo/Tables/dbo.WashingtonLegislativeDistrict.sql
+++ WADNRForestHealthTracker.Database/dbo/Tables/dbo.WashingtonLegislativeDistrict.sql
@@ -1,31 +1,13 @@
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE [dbo].[WashingtonLegislativeDistrict](
-	[WashingtonLegislativeDistrictID] [int] IDENTITY(1,1) NOT NULL,
-	[WashingtonLegislativeDistrictLocation] [geometry] NOT NULL,
-	[WashingtonLegislativeDistrictNumber] [int] NOT NULL,
-	[WashingtonLegislativeDistrictName] [varchar](200) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
- CONSTRAINT [PK_WashingtonLegislativeDistrict_WashingtonLegislativeDistrictID] PRIMARY KEY CLUSTERED 
-(
-	[WashingtonLegislativeDistrictID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-
-GO
-SET ARITHABORT ON
-SET CONCAT_NULL_YIELDS_NULL ON
-SET QUOTED_IDENTIFIER ON
-SET ANSI_NULLS ON
-SET ANSI_PADDING ON
-SET ANSI_WARNINGS ON
-SET NUMERIC_ROUNDABORT OFF
-
+    [WashingtonLegislativeDistrictID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_WashingtonLegislativeDistrict_WashingtonLegislativeDistrictID] PRIMARY KEY,
+    [WashingtonLegislativeDistrictLocation] [geometry] NOT NULL,
+    [WashingtonLegislativeDistrictNumber] [int] NOT NULL,
+    [WashingtonLegislativeDistrictName] [varchar](200) NOT NULL
+)
 GO
 CREATE SPATIAL INDEX [SPATIAL_WashingtonLegislativeDistrict_WashingtonLegislativeDistrictLocation] ON [dbo].[WashingtonLegislativeDistrict]
 (
-	[WashingtonLegislativeDistrictLocation]
-)USING  GEOMETRY_AUTO_GRID 
-WITH (BOUNDING_BOX =(-125, 45, -116, 50), 
-CELLS_PER_OBJECT = 8, PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+    [WashingtonLegislativeDistrictLocation]
+)USING GEOMETRY_AUTO_GRID 
+WITH (BOUNDING_BOX =(-125, 45, -116, 50), CELLS_PER_OBJECT = 8) 
+GO
