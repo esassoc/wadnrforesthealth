dda486e03e27da53772d5b21421ec2f0419071ee
tomkamin <tom.kamin@sitkatech.com>
2025-09-04 14:03:44 -0700

    WADNR-2189 Move FHT to .NET Core adding and updating the table scripts tmp* tables 3

--- WADNRForestHealthTracker.Database/dbo/Tables/dbo.tmpAgreementContact.sql
+++ WADNRForestHealthTracker.Database/dbo/Tables/dbo.tmpAgreementContact.sql
@@ -1,20 +1,13 @@
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE [dbo].[tmpAgreementContact](
-	[AgreementContactID] [int] IDENTITY(1,1) NOT NULL,
-	[AgreementNumber] [nvarchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[Title] [nvarchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[FirstName] [nvarchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[LastName] [nvarchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[EmailAddress] [nvarchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[Organization] [nvarchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[AgreementRole] [nvarchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[F8] [nvarchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[F9] [nvarchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
- CONSTRAINT [PK_tmpAgreementContact_AgreementContactID] PRIMARY KEY CLUSTERED 
-(
-	[AgreementContactID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+    [AgreementContactID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_TmpAgreementContact_AgreementContactID] PRIMARY KEY,
+    [AgreementNumber] [varchar](255) NULL,
+    [Title] [varchar](255) NULL,
+    [FirstName] [varchar](255) NULL,
+    [LastName] [varchar](255) NULL,
+    [EmailAddress] [varchar](255) NULL,
+    [Organization] [varchar](255) NULL,
+    [AgreementRole] [varchar](255) NULL,
+    [F8] [varchar](255) NULL,
+    [F9] [varchar](255) NULL
+)
+GO
