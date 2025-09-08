08a84fe13b74702efeda7312e1d28d746e40c438
tomkamin <tom.kamin@sitkatech.com>
2025-09-08 11:57:52 -0700

    WADNR-2189 Move FHT to .NET Core updating HangFire schema tables 2

--- WADNRForestHealthTracker.Database/HangFire/Tables/HangFire.Job.sql
+++ WADNRForestHealthTracker.Database/HangFire/Tables/HangFire.Job.sql
@@ -1,26 +1,11 @@
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE [HangFire].[Job](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[StateId] [int] NULL,
-	[StateName] [nvarchar](20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[InvocationData] [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	[Arguments] [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	[CreatedAt] [datetime] NOT NULL,
-	[ExpireAt] [datetime] NULL,
- CONSTRAINT [PK_Job_Id] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-
+    [Id] [int] IDENTITY(1,1) NOT NULL CONSTRAINT PK_Job_Id PRIMARY KEY,
+    [StateId] [int] NULL,
+    [StateName] [varchar](20) NULL,
+    [InvocationData] [varchar](max) NOT NULL,
+    [Arguments] [varchar](max) NOT NULL,
+    [CreatedAt] [datetime] NOT NULL,
+    [ExpireAt] [datetime] NULL
+)
 GO
-SET ANSI_PADDING ON
-
-GO
-CREATE NONCLUSTERED INDEX [IX_HangFire_Job_StateName] ON [HangFire].[Job]
-(
-	[StateName] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+CREATE INDEX IX_HangFire_Job_StateName ON [HangFire].[Job]([StateName]);
