c512574e8e851315712d13c93a9f78266788ba79
tomkamin <tom.kamin@sitkatech.com>
2025-09-17 12:01:18 -0700

    WADNR-2189 Move FHT to .NET Core fixing table scripts for FieldDefinition*

--- WADNRForestHealthTracker.Database/dbo/Tables/dbo.ProjectUpdateSection.sql
+++ WADNRForestHealthTracker.Database/dbo/Tables/dbo.ProjectUpdateSection.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [dbo].[ProjectUpdateSection](
     [ProjectUpdateSectionID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_ProjectUpdateSection_ProjectUpdateSectionID] PRIMARY KEY,
     [ProjectUpdateID] [int] NOT NULL CONSTRAINT [FK_ProjectUpdateSection_ProjectUpdate_ProjectUpdateID] FOREIGN KEY REFERENCES [dbo].[ProjectUpdate]([ProjectUpdateID]),
-    [ProjectWorkflowSectionID] [int] NOT NULL CONSTRAINT [FK_ProjectUpdateSection_ProjectWorkflowSection_ProjectWorkflowSectionID] FOREIGN KEY REFERENCES [dbo].[ProjectWorkflowSection]([ProjectWorkflowSectionID]),
+    [ProjectWorkflowSectionGroupingID] [int] NOT NULL CONSTRAINT [FK_ProjectUpdateSection_ProjectWorkflowSectionGrouping_ProjectWorkflowSectionGroupingID] FOREIGN KEY REFERENCES [dbo].[ProjectWorkflowSectionGrouping]([ProjectWorkflowSectionGroupingID]),
     [ProjectUpdateSectionContent] [dbo].[html] NULL
 )
 GO
