0ec3b4c8deecc17892b81cb1a7c031656f6d0b14
tomkamin <tom.kamin@sitkatech.com>
2025-09-17 12:16:31 -0700

    WADNR-2189 Move FHT to .NET Core fixing table scripts for dbo.County

--- WADNRForestHealthTracker.Database/dbo/Tables/dbo.County.sql
+++ WADNRForestHealthTracker.Database/dbo/Tables/dbo.County.sql
@@ -1,9 +1,10 @@
-CREATE TABLE dbo.County
-(
-    CountyID int IDENTITY(1,1) NOT NULL CONSTRAINT PK_County_CountyID PRIMARY KEY,
-    CountyName varchar(100) NOT NULL,
-    StateProvinceID int NOT NULL CONSTRAINT FK_County_StateProvince_StateProvinceID FOREIGN KEY REFERENCES dbo.StateProvince(StateProvinceID),
-    CountyCode varchar(10) NOT NULL,
-    CONSTRAINT AK_County_CountyName UNIQUE (CountyName)
+CREATE TABLE [dbo].[County](
+    [CountyName] [varchar](100) NOT NULL,
+    [StateProvinceID] [int] NOT NULL CONSTRAINT FK_County_StateProvince_StateProvinceID FOREIGN KEY REFERENCES [dbo].[StateProvince]([StateProvinceID]),
+    [CountyFeature] [geometry] NULL,
+    [CountyID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT PK_County_CountyID PRIMARY KEY,
+    CONSTRAINT AK_County_CountyName_StateProvinceID UNIQUE ([CountyName], [StateProvinceID])
 )
 GO
+--CREATE SPATIAL INDEX SPATIAL_County_CountyFeature ON [dbo].[County]([CountyFeature]) USING GEOMETRY_AUTO_GRID WITH (BOUNDING_BOX =(-125, 45, -116, 50), CELLS_PER_OBJECT = 8);
+--GO
