a26f32dc5aa11ab279b4b3833f18ff63b8a922a0
tomkamin <tom.kamin@sitkatech.com>
2025-09-17 16:22:13 -0700

    WADNR-2189 Move FHT to .NET Core fixing table scripts for ArcOnlineFinanceApiRawJsonImport and TabularDataImport

--- WADNRForestHealthTracker.Database/dbo/Tables/dbo.ArcOnlineFinanceApiRawJsonImport.sql
+++ WADNRForestHealthTracker.Database/dbo/Tables/dbo.ArcOnlineFinanceApiRawJsonImport.sql
@@ -1,8 +1,11 @@
-CREATE TABLE dbo.ArcOnlineFinanceApiRawJsonImport
-(
-    ArcOnlineFinanceApiRawJsonImportID int IDENTITY(1,1) NOT NULL CONSTRAINT PK_ArcOnlineFinanceApiRawJsonImport_ArcOnlineFinanceApiRawJsonImportID PRIMARY KEY,
-    ArcOnlineFinanceApiRawJsonImportTableTypeID int NOT NULL CONSTRAINT FK_ArcOnlineFinanceApiRawJsonImport_ArcOnlineFinanceApiRawJsonImportTableType_ArcOnlineFinanceApiRawJsonImportTableTypeID FOREIGN KEY REFERENCES dbo.ArcOnlineFinanceApiRawJsonImportTableType(ArcOnlineFinanceApiRawJsonImportTableTypeID),
-    JsonRaw varchar(max) NOT NULL,
-    ImportDate datetime NOT NULL
+CREATE TABLE [dbo].[ArcOnlineFinanceApiRawJsonImport](
+    [ArcOnlineFinanceApiRawJsonImportID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT PK_ArcOnlineFinanceApiRawJsonImport_ArcOnlineFinanceApiRawJsonImportID PRIMARY KEY,
+    [CreateDate] [datetime] NOT NULL,
+    [ArcOnlineFinanceApiRawJsonImportTableTypeID] [int] NOT NULL CONSTRAINT FK_ArcOnlineFinanceApiRawJsonImport_ArcOnlineFinanceApiRawJsonImportTableType_ArcOnlineFinanceApiRawJsonImportTableTypeID FOREIGN KEY REFERENCES [dbo].[ArcOnlineFinanceApiRawJsonImportTableType]([ArcOnlineFinanceApiRawJsonImportTableTypeID]),
+    [BienniumFiscalYear] [int] NULL,
+    [FinanceApiLastLoadDate] [datetime] NULL,
+    [RawJsonString] [varchar](max) NOT NULL,
+    [JsonImportDate] [datetime] NULL,
+    [JsonImportStatusTypeID] [int] NOT NULL CONSTRAINT FK_ArcOnlineFinanceApiRawJsonImport_JsonImportStatusType_JsonImportStatusTypeID FOREIGN KEY REFERENCES [dbo].[JsonImportStatusType]([JsonImportStatusTypeID])
 )
 GO
--- WADNRForestHealthTracker.Database/dbo/Tables/dbo.TabularDataImport.sql
+++ WADNRForestHealthTracker.Database/dbo/Tables/dbo.TabularDataImport.sql
@@ -1,10 +1,9 @@
 CREATE TABLE [dbo].[TabularDataImport](
-	[TabularDataImportID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_TabularDataImport_TabularDataImportID] PRIMARY KEY,
-	[TabularDataImportTableTypeID] [int] NOT NULL CONSTRAINT [FK_TabularDataImport_TabularDataImportTableType_TabularDataImportTableTypeID] FOREIGN KEY REFERENCES [dbo].[TabularDataImportTableType]([TabularDataImportTableTypeID]),
-	[TabularDataImportName] [varchar](200) NOT NULL,
-	[TabularDataImportDisplayName] [varchar](200) NOT NULL,
-	[TabularDataImportDescription] [varchar](max) NULL,
-	[TabularDataImportDate] [datetime] NOT NULL,
-	[TabularDataImportStatusTypeID] [int] NOT NULL CONSTRAINT [FK_TabularDataImport_TabularDataImportStatusType_TabularDataImportStatusTypeID] FOREIGN KEY REFERENCES [dbo].[TabularDataImportStatusType]([TabularDataImportStatusTypeID])
+	[TabularDataImportID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT PK_TabularDataImport_TabularDataImportID PRIMARY KEY,
+	[TabularDataImportTableTypeID] [int] NOT NULL CONSTRAINT FK_TabularDataImport_TabularDataImportTableType_TabularDataImportTableTypeID FOREIGN KEY REFERENCES [dbo].[TabularDataImportTableType]([TabularDataImportTableTypeID]),
+	[UploadDate] [datetime] NULL,
+	[UploadPersonID] [int] NULL CONSTRAINT FK_TabularDataImport_Person_UploadPersonID_PersonID FOREIGN KEY REFERENCES [dbo].[Person]([PersonID]),
+	[LastProcessedDate] [datetime] NULL,
+	[LastProcessedPersonID] [int] NULL CONSTRAINT FK_TabularDataImport_Person_LastProcessedPersonID_PersonID FOREIGN KEY REFERENCES [dbo].[Person]([PersonID])
 )
 GO
