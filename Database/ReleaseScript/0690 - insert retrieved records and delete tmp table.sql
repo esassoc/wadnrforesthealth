3bb42bbf49df08f02434ce9fbc2ca7c1d1e02ed0
tomkamin <tom.kamin@sitkatech.com>
2025-10-09 19:13:33 -0700

    WADNR-2206 Recover missing fund sources adding a join to the script so missing projects do not cause script to fail

--- Database/ReleaseScript/0690 - insert retrieved records and delete tmp table.sql
+++ Database/ReleaseScript/0690 - insert retrieved records and delete tmp table.sql
@@ -17,13 +17,51 @@ insert into dbo.ProjectFundSourceAllocationRequest (ProjectID, FundSourceAllocat
 		tmpgar.ImportedFromTabularData
 	from
 	dbo.tmpProjectGrantAllocationRequestBackup as tmpgar
+	join dbo.Project as p on p.ProjectID = tmpgar.ProjectID
 	where
 	ProjectGrantAllocationRequestID not in (select 
-	pgar.ProjectGrantAllocationRequestID 
-from 
-	dbo.tmpProjectGrantAllocationRequestBackup as pgar
-	join dbo.ProjectFundSourceAllocationRequest as fsar on pgar.ProjectID = fsar.ProjectID and pgar.GrantAllocationID = fsar.FundSourceAllocationID)
+												pgar.ProjectGrantAllocationRequestID 
+											from 
+												dbo.tmpProjectGrantAllocationRequestBackup as pgar
+												join dbo.ProjectFundSourceAllocationRequest as fsar on pgar.ProjectID = fsar.ProjectID and pgar.GrantAllocationID = fsar.FundSourceAllocationID)
 
 
 
 	drop table dbo.tmpProjectGrantAllocationRequestBackup
+
+
+
+/*
+2023-SPS-0014
+2025-NWS-0012
+
+
+	select 
+		p.ProjectName,
+		fsa.FundSourceAllocationName,
+		tmpgar.ProjectID as ProjectID,
+		tmpgar.GrantAllocationID as FundSourceAllocationID,
+		tmpgar.TotalAmount as TotalAmount,
+		tmpgar.MatchAmount,
+		tmpgar.PayAmount,
+		tmpgar.CreateDate,
+		tmpgar.UpdateDate,
+		tmpgar.ImportedFromTabularData
+	from
+		dbo.tmpProjectGrantAllocationRequestBackup as tmpgar
+		join dbo.Project as p on p.ProjectID = tmpgar.ProjectID
+		join dbo.FundSourceAllocation as fsa on fsa.FundSourceAllocationID = tmpgar.GrantAllocationID
+	where
+
+		p.ProjectName in ('2023-SPS-0014','2025-NWS-0012')
+
+		ProjectGrantAllocationRequestID not in (select 
+													pgar.ProjectGrantAllocationRequestID 
+												from 
+													dbo.tmpProjectGrantAllocationRequestBackup as pgar
+													join dbo.ProjectFundSourceAllocationRequest as fsar on pgar.ProjectID = fsar.ProjectID and pgar.GrantAllocationID = fsar.FundSourceAllocationID)
+
+
+
+
+*/
