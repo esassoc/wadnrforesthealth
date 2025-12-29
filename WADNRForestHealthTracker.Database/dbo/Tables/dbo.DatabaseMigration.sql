6edf4044d743561ac33df106ee4cc86040045705
raynardlee <raynardlee@yahoo.com>
2025-12-29 13:43:18 -0800

    updating schema for DatabaseMigration

--- WADNRForestHealthTracker.Database/dbo/Tables/dbo.DatabaseMigration.sql
+++ WADNRForestHealthTracker.Database/dbo/Tables/dbo.DatabaseMigration.sql
@@ -1,7 +1,9 @@
-CREATE TABLE dbo.DatabaseMigration
-(
-    DatabaseMigrationID int IDENTITY(1,1) NOT NULL CONSTRAINT PK_DatabaseMigration_DatabaseMigrationID PRIMARY KEY,
-    MigrationName varchar(100) NOT NULL,
-    MigrationDate datetime NOT NULL
-)
-GO
+CREATE TABLE [dbo].[DatabaseMigration] (
+    [DatabaseMigrationNumber]	INT				NOT NULL IDENTITY(1,1),
+    [ReleaseScriptFileName]		VARCHAR(500)	NOT NULL,
+    [DateMigrated]				DATETIME2		NOT NULL DEFAULT(GETUTCDATE()),
+    [MigrationAuthorName]		VARCHAR(200)	NOT NULL,
+    [MigrationReason]			VARCHAR(MAX)	NULL
+    CONSTRAINT [PK_DatabaseMigration_DatabaseMigrationNumber]	PRIMARY KEY CLUSTERED ([DatabaseMigrationNumber] ASC),
+    CONSTRAINT [UC_DatabaseMigration_ReleaseScriptFileName]		UNIQUE NONCLUSTERED ([ReleaseScriptFileName] ASC),
+);
