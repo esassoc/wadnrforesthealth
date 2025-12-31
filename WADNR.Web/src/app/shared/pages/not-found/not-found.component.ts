a73c1602648587ea62af69b4d6929e46be042fa5
Mack Peters <mpeters@esassoc.com>
2025-12-31 14:31:27 -0800

    WADNR-2210 Navs setup -Might change internal layout later, as I'm not sure if we need sitelayout without areas? -Also imported a few images and colors -Updated .gitignore to ignore wwwroot

--- WADNR.Web/src/app/shared/pages/not-found/not-found.component.ts
+++ WADNR.Web/src/app/shared/pages/not-found/not-found.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { PageHeaderComponent } from "../../components/page-header/page-header.component";
-import { SiteLayoutComponent } from "src/app/pages/site-layout/site-layout.component";
 
 @Component({
     selector: "not-found",
     templateUrl: "./not-found.component.html",
     styleUrls: ["./not-found.component.scss"],
-    imports: [PageHeaderComponent, SiteLayoutComponent]
+    imports: [PageHeaderComponent],
 })
 export class NotFoundComponent implements OnInit {
     constructor() {}
