abf85477d5b7fb35e43c401a24ac573a2e2bbf80
tomkamin <tom.kamin@sitkatech.com>
2026-07-02 17:24:51 -0700

    WADNR-2272 Fix issues discovered in bulk import adding front end logging for datadog

--- WADNR.Web/src/environments/environment.prod.ts
+++ WADNR.Web/src/environments/environment.prod.ts
@@ -4,7 +4,12 @@ export const environment = {
     dev: false,
     mainAppApiUrl: "https://internalapi-wadnr.esa-prod.sitkatech.com",
     geoserverMapServiceUrl: "https://geoserver-wadnr.esa-prod.sitkatech.com/geoserver/WADNRForestHealth",
-    // datadogClientToken: "pub6bc5bcb39be6b4c926271a35cb8cb46a",
+    datadog: {
+        clientToken: "pub6bc5bcb39be6b4c926271a35cb8cb46a",
+        site: "datadoghq.com",
+        service: "wadnr-web",
+        env: "prod",
+    },
     auth0: {
         domain: "wadnr.us.auth0.com",
         clientId: "q7AHuE3OqhPErLAQm8J1RWowIPJoKEay",
